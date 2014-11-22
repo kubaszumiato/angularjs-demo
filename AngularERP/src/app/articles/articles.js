00e68c4e0b4d8afafe097f12939255b90d570920
KI <izdebski.k@hotmail.com>
2014-11-22 16:42:19 +0100

    articles service

--- AngularERP/src/app/articles/articles.js
+++ AngularERP/src/app/articles/articles.js
@@ -1,12 +1,13 @@
-//use strict';
+//'use strict';
 var myapp = angular.module('ngBoilerplate.articles', [
     'ui.router',
     'placeholders',
-    'ui.bootstrap'
+    'ui.bootstrap' 
 ]);
 
 myapp.config(function config($stateProvider) {
-        $stateProvider.state('articles', {
+    $stateProvider
+        .state('articles', {
             url: '/articles',
             views: {
                 "main": {
@@ -15,30 +16,23 @@ myapp.config(function config($stateProvider) {
                 }
             },
             data: { pageTitle: 'articles' }
-        },
-        {
-            url: '/articles/details',
+        })
+        .state('details', {
+            url: '/articles/:id',
             views: {
-                "details": {
-                    controller: 'ArticlesCtrl',
-                    templateUrl: 'articles/details.tpl.html'
+                "main": {
+                    controller: 'ArticleDetailsCtrl',
+                    templateUrl: 'articles/articleDetails.tpl.html'
                 }
             },
-            data: { pageTitle: 'details' }
-        }
-        );
-        //$stateProvider.state('articles', {
-        //    url: '/articles/details',
-        //    views: {
-        //        "details": {
-        //            controller: 'ArticlesCtrl',
-        //            templateUrl: 'articles/details.tpl.html'
-        //        }
-        //    },
-        //    data: { pageTitle: 'details' }
-        //});
-    })
-    .controller('ArticlesCtrl', function ArticlesCtrl($scope, ArticleService) {
+            data: { pageTitle: 'article details page' }
+        });
+
+});
+ 
+ 
+
+myapp.controller('ArticlesCtrl', function ArticlesCtrl($scope, ArticleService, $state) {
         // This is simple a demo for UI Boostrap.
         console.log('ArticlesCtrl controller loaded sd');
     $scope.articles = []; 
@@ -57,10 +51,31 @@ myapp.config(function config($stateProvider) {
 
 
         };
+    $scope.getArticleDetails = function(article) {
+        $state.go('details', { id: article.id }, { reload: true });
+    };
 
         $scope.refreshArticles();
         console.log('xxx');
-    });
+});
+
+myapp.controller('ArticleDetailsCtrl', function ArticleDetailsCtrl($scope, $stateParams, ArticleService) {
+    // This is simple a demo for UI Boostrap.
+    console.log('jhvhjg  ' + $stateParams.id);
+    console.log('ArticleDetailsCtrl controller loaded sd');
+    $scope.article = [];
+    $scope.loadDetails = function () {
+        console.log('loadDetails');
+        ArticleService.getArticlebyId($stateParams.id,function (data) {
+            $scope.article = data;
+        });
+
+
+    };
+
+    $scope.loadDetails();
+    console.log('ArticleDetailsCtrl');
+});
 
 myapp.service("ArticleService", function($http, $location) {
     var ArticleService = {};
@@ -84,12 +99,17 @@ myapp.service("ArticleService", function($http, $location) {
 
     };
 
-    ArticleService.getArticlestmp = function () {
+    ArticleService.getArticlebyId = function (id, callback) {
+        console.log('ArticleService.getArticlebyId');
 
+        $http.get('assets/ArticleDetails.json').success(function (data) {
+            console.log('success get');
+            console.log(data[id]);
+            callback(data[id]);
+        });
     };
 
     return ArticleService;
 });
 
-
-
+ 
