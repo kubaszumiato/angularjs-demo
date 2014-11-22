ee8ce5c42a387ea51a0c96cb80f8f12ab3777c31
krzysiekchudzinski <krzysiekchudzinski@users.noreply.github.com>
2014-11-22 12:13:30 +0100

    initial commit for customers

---
+++ AngularERP/src/app/customers/customers.js
@@ -0,0 +1,29 @@
+﻿angular.module('ngBoilerplate.customers', [
+  'ui.router',
+  'placeholders',
+  'ui.bootstrap'
+])
+
+.config(function config($stateProvider) {
+    $stateProvider.state('customers', {
+        url: '/customers',
+        views: {
+            "main": {
+                controller: 'CustomersController',
+                templateUrl: 'customers/customers.tpl.html'
+            }
+        },
+        data: { pageTitle: 'Customers' }
+    });
+})
+
+.controller('CustomersController', function CustomersController($scope) {
+    // This is simple a demo for UI Boostrap.
+    $scope.dropdownDemoItems = [
+      "The first choi ce!",
+      "And another choice for you.",
+      "but wait! A third!"
+    ];
+})
+
+;
