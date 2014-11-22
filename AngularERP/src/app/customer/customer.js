74bbce7a6b230c624e23c841e063943ade796119
fenomen <marek.fenomen@gmail.com>
2014-11-22 15:46:22 +0100

    updated

--- AngularERP/src/app/customer/customer.js
+++ AngularERP/src/app/customer/customer.js
@@ -17,33 +17,16 @@
     });
 })
 
-<<<<<<< HEAD
-.controller('customerController', function customerController($scope, $http) {
-    // This is simple a demo for UI Boostrap.
-    //$scope.dropdownDemoItems = [
-    //  "The first choi ce!",
-    //  "And another choice for you.",
-    //  "but wait! A third!"
-    //];
-=======
 .controller('customerController', function customerController($scope, $stateParams) {
->>>>>>> ff956bd38b3519b65e06b2195d979ec1f65086c1
+
 
         var index = $stateParams.customerIndex;
         $scope.Index = index;
 
-<<<<<<< HEAD
     //var cust = $scope.customers[0];
-    $http.get('customers/customer', id).success(function (data) {
-        $scope.customer = data;
+    //$http.get('customers/customer', id).success(function (data) {
+    //    $scope.customer = data;
     });
 
 
 
-
-=======
-    //w zmiennej index mamy index customera
->>>>>>> ff956bd38b3519b65e06b2195d979ec1f65086c1
-})
-
-;
